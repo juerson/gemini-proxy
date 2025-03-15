abac352490160264c9da38dee0081d48b71a6e98
Touchumind <chinash2010@gmail.com>
2025-03-16 00:54:10 +0800

    save

--- worker.js
+++ worker.js
@@ -8,48 +8,55 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-
 export default {
   async fetch(request, env) {
     // hack
-    await env.DB.exec('SELECT * FROM Example LIMIT 1');
+    await env.DB.exec("SELECT * FROM Example LIMIT 1");
 
-    const url = new URL(request.url)
-    let targetURL = new URL('https://generativelanguage.googleapis.com')
-  
-    targetURL.pathname = url.pathname
-    targetURL.search = url.search
-  
+    const url = new URL(request.url);
+    if (url.pathname === "/") {
+      return new Response("Hello", {
+        status: 200,
+      });
+    }
+
+    let targetURL = new URL("https://generativelanguage.googleapis.com");
+
+    targetURL.pathname = url.pathname;
+    targetURL.search = url.search;
+
     let newRequest = new Request(targetURL, {
       method: request.method,
       headers: request.headers,
-      body: request.body
-    })
-  
-    let response = await fetch(newRequest)
-  
+      body: request.body,
+    });
+
+    let response = await fetch(newRequest);
+
     // 添加跨域支持
     let corsHeaders = {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
-      'Access-Control-Allow-Headers': request.headers.get('Access-Control-Request-Headers'),
-    }
-  
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+      "Access-Control-Allow-Headers": request.headers.get(
+        "Access-Control-Request-Headers"
+      ),
+    };
+
     // 如果是预检请求，直接返回跨域头
-    if (request.method === 'OPTIONS') {
-      return new Response(null, { headers: corsHeaders })
+    if (request.method === "OPTIONS") {
+      return new Response(null, { headers: corsHeaders });
     }
-  
+
     // 复制响应以添加新的头
-    let responseHeaders = new Headers(response.headers)
+    let responseHeaders = new Headers(response.headers);
     for (let [key, value] of Object.entries(corsHeaders)) {
-      responseHeaders.set(key, value)
+      responseHeaders.set(key, value);
     }
-  
+
     return new Response(response.body, {
       status: response.status,
       statusText: response.statusText,
-      headers: responseHeaders
-    })
-  }
-}
+      headers: responseHeaders,
+    });
+  },
+};
