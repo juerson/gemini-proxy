e2ec9183d95ad07830ec35b28fb990d46d209931
Touchumind <wuxueqian2010@hotmail.com>
2025-03-31 16:14:04 +0800

    feat: 通过 durable object 特性来限定代理源位置

--- worker.js
+++ worker.js
@@ -1,37 +1,38 @@
+import { DurableObject } from "cloudflare:workers";
+
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
+ * Env provides a mechanism to reference bindings declared in wrangler.jsonc within JavaScript
  *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
+ * @typedef {Object} Env
+ * @property {DurableObjectNamespace} MY_DURABLE_OBJECT - The Durable Object namespace binding
  */
+
+/** A Durable Object's behavior is defined in an exported Javascript class */
+export class GeminiProxy extends DurableObject {
+	/**
+	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
+	 * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
+	 *
+	 * @param {DurableObjectState} ctx - The interface for interacting with Durable Object state
+	 * @param {Env} env - The interface to reference bindings declared in wrangler.jsonc
+	 */
+	constructor(ctx, env) {
+		super(ctx, env);
+	}
+
+	async fetch(req) {
+		return fetch(req);
+	}
+}
 
 export default {
   async fetch(request, env) {
-    // hack
-    await env.DB.exec("SELECT * FROM Example LIMIT 1");
-
     const url = new URL(request.url);
     if (url.pathname === "/") {
       return new Response("Hello", {
         status: 200,
       });
     }
-
-    let targetURL = new URL("https://generativelanguage.googleapis.com");
-
-    targetURL.pathname = url.pathname;
-    targetURL.search = url.search;
-
-    let newRequest = new Request(targetURL, {
-      method: request.method,
-      headers: request.headers,
-      body: request.body,
-    });
-
-    let response = await fetch(newRequest);
 
     // 添加跨域支持
     let corsHeaders = {
@@ -46,6 +47,24 @@ export default {
     if (request.method === "OPTIONS") {
       return new Response(null, { headers: corsHeaders });
     }
+
+		let targetURL = new URL("https://generativelanguage.googleapis.com");
+
+    targetURL.pathname = url.pathname;
+    targetURL.search = url.search;
+
+		let newRequest = new Request(targetURL, {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
+    });
+
+		const id = env.MY_DURABLE_OBJECT.idFromName("gemini");
+
+		// https://developers.cloudflare.com/durable-objects/reference/data-location/
+		const stub = env.MY_DURABLE_OBJECT.get(id, { locationHint: "wnam" });
+
+		let response = await stub.fetch(newRequest);
 
     // 复制响应以添加新的头
     let responseHeaders = new Headers(response.headers);
